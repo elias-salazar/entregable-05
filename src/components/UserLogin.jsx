306f9e9bd150a4c5c8818dec25f281433a25fe53
elias-salazar <eliotssalazarcampos@gmail.com>
2022-09-29 22:18:42 -0300

    probando

--- src/components/UserLogin.jsx
+++ src/components/UserLogin.jsx
@@ -18,7 +18,7 @@ const UserLogin = () => {
     <div className="content-login">
       <div className="container-input">
         <div className="img-pokedex">
-          <img src="src/images/Pokedex.PNG" alt="pokedex" />
+          <img src="Pokedex.png" alt="pokedex" />
         </div>
         <div className="content-hi-login">
           <h1>¡Hola entrenador!</h1>
